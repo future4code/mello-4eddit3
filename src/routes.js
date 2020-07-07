fa9bc540ff5ce8cec5d8aab0dc391a0dc126af52
Jonatan Machado <jonatanmachado35@gmail.com>
2020-07-07 16:07:31 -0300

    register concluido

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Login from './components/login';
+import Register from './pages/register';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={Login} />
+      <Route path="/registro" component={Register} />
       {/* <Route path="/login" component={} />
       <Route path="/cadastro" component={} />
       <Route path="/posts" component={} />
