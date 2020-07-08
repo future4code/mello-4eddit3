708636cb005241e8eac0987d0e5a4632fa128689
tatianafischer <tatiana.fischer@acad.pucrs.br>
2020-07-08 15:01:51 -0300

    pagina de detalhes do post e input para comentário

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Feed from './pages/feed';
-
 import Login from './pages/login';
+import Post from './pages/postdetail';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={Login} />
-      <Route path="/posts" exact component={Feed} />
+      <Route path="/feed" exact component={Feed} />
+      <Route path="/posts/:id">
+        <Post />
+      </Route>
       {/* <Route path="/login" component={} />
       <Route path="/cadastro" component={} />
 
-      <Route path="/post:id" component={} /> */}
+       */}
     </Switch>
   );
 }
